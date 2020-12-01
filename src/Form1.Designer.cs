3f29d552370761d189d0b2a90716ac8ca9959bb9
Bernardas <bzxmixrox@gmail.com>
2020-12-01 21:11:13 +0200

    .

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace NoteSaver
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button_next = new System.Windows.Forms.Button();
+			this.button_previous = new System.Windows.Forms.Button();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.AcceptsTab = true;
+			this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.richTextBox1.EnableAutoDragDrop = true;
+			this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+			this.richTextBox1.Location = new System.Drawing.Point(71, 29);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(864, 507);
+			this.richTextBox1.TabIndex = 1;
+			this.richTextBox1.Text = "";
+			this.richTextBox1.WordWrap = false;
+			this.richTextBox1.ContentsResized += new System.Windows.Forms.ContentsResizedEventHandler(this.richTextBox1_ContentsResized);
+			this.richTextBox1.VScroll += new System.EventHandler(this.richTextBox1_VScroll);
+			// 
+			// button1
+			// 
+			this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.button1.Location = new System.Drawing.Point(0, 513);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(210, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Save";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+			this.splitContainer1.Panel1.Controls.Add(this.button1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.richTextBox2);
+			this.splitContainer1.Panel2.Controls.Add(this.button_previous);
+			this.splitContainer1.Panel2.Controls.Add(this.button_next);
+			this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+			this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+			this.splitContainer1.Size = new System.Drawing.Size(1152, 536);
+			this.splitContainer1.SplitterDistance = 210;
+			this.splitContainer1.TabIndex = 3;
+			// 
+			// treeView1
+			// 
+			this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.treeView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.treeView1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+			this.treeView1.ItemHeight = 20;
+			this.treeView1.Location = new System.Drawing.Point(4, 3);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(203, 504);
+			this.treeView1.TabIndex = 3;
+			this.treeView1.BeforeCollapse += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeCollapse);
+			this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+			this.treeView1.DoubleClick += new System.EventHandler(this.treeView1_DoubleClick);
+			this.treeView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseDown);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Location = new System.Drawing.Point(71, 3);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(702, 20);
+			this.textBox1.TabIndex = 2;
+			this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+			// 
+			// button_next
+			// 
+			this.button_next.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button_next.Location = new System.Drawing.Point(860, 3);
+			this.button_next.Name = "button_next";
+			this.button_next.Size = new System.Drawing.Size(75, 20);
+			this.button_next.TabIndex = 3;
+			this.button_next.Text = "Next";
+			this.button_next.UseVisualStyleBackColor = true;
+			// 
+			// button_previous
+			// 
+			this.button_previous.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button_previous.Location = new System.Drawing.Point(779, 3);
+			this.button_previous.Name = "button_previous";
+			this.button_previous.Size = new System.Drawing.Size(75, 20);
+			this.button_previous.TabIndex = 4;
+			this.button_previous.Text = "Previous";
+			this.button_previous.UseVisualStyleBackColor = true;
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.richTextBox2.BackColor = System.Drawing.SystemColors.Control;
+			this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.richTextBox2.Enabled = false;
+			this.richTextBox2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.richTextBox2.Location = new System.Drawing.Point(3, 29);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+			this.richTextBox2.Size = new System.Drawing.Size(69, 507);
+			this.richTextBox2.TabIndex = 5;
+			this.richTextBox2.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1176, 560);
+			this.Controls.Add(this.splitContainer1);
+			this.MinimumSize = new System.Drawing.Size(900, 400);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button_previous;
+		private System.Windows.Forms.Button button_next;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+	}
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace NoteSaver
+{
+	public partial class Form1 : Form
+	{
+		private bool isDoubleClick = false;
+		private int textSelection = 0;
+		private int rowNumber = 0;
+		private int previousLineIndex = 0;
+
+
+		public Form1()
+		{
+			InitializeComponent();
+			LoadText();
+		}
+
+		private void LoadText()
+		{
+			if (richTextBox1 != null && richTextBox1.Text.Length > 0)
+			{
+				richTextBox1.Text = "";
+			}
+
+			if (treeView1 != null && treeView1.Nodes.Count > 0)
+			{
+				treeView1.Nodes.Clear();
+			}
+
+			try
+			{
+				richTextBox1.LoadFile(Path.GetPath(), RichTextBoxStreamType.RichText);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+
+			rowNumber = 0;
+			var level = 1;
+
+			var lines = richTextBox1.Lines
+				.Select((x, index) => new Line 
+				{ 
+					Index = index, 
+					Text = x 
+				})
+				.ToList();
+
+			richTextBox2.Lines = lines.Select(x => (x.Index + 1).ToString()).ToArray();
+
+			treeView1.Nodes.AddRange(MakeTreeNode(lines, level, lines.Count() - 1));
+
+			treeView1.ExpandAll();
+
+			if (textSelection != 0 || textSelection != -1)
+			{
+				richTextBox1.Select(textSelection, 0);
+				richTextBox1.ScrollToCaret();
+			}
+		}
+
+		private TreeNode[] MakeTreeNode(IEnumerable<Line> lines, int level, int sectionEnd)
+		{
+			ICollection<TreeNode> listTreeNode = new List<TreeNode>();
+
+			var listLines = lines
+				.Where(x => x.Text.StartsWith($"##{level}") && x.Index >= rowNumber && x.Index < sectionEnd)
+				.ToList();
+
+			for (int i = 0; i < listLines.Count(); i++)
+			{
+				var LevelLines = lines
+					.Where(x => x.Text.StartsWith($"##{level}") && x.Index >= rowNumber && x.Index < sectionEnd)
+					.Take(2)
+					.ToList();
+
+				var firstRow = LevelLines.FirstOrDefault();
+				var lastRow = LevelLines.Count() > 1 ? LevelLines.LastOrDefault() : null;
+				var treeNodes = MakeTreeNode(lines, level + 1, lastRow != null ? lastRow.Index : sectionEnd);
+
+				if (treeNodes.Length != 0)
+				{
+					listTreeNode.Add(new TreeNode(firstRow.Text.Replace($"##{level}", ""), treeNodes));
+				}
+				else
+				{
+					listTreeNode.Add(new TreeNode(firstRow.Text.Replace($"##{level}", "")));
+				}
+
+				rowNumber = lastRow != null ? lastRow.Index : listLines[i].Index;
+			}
+
+			return listTreeNode.ToArray();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			richTextBox1.SaveFile(Path.GetPath(), RichTextBoxStreamType.RichText);
+			LoadText();
+		}
+
+		private void treeView1_DoubleClick(object sender, EventArgs e)
+		{
+			int selectionIndex = richTextBox1.Find(treeView1.SelectedNode.Text.Replace("\r\n", ""));
+
+			if (treeView1.SelectedNode != null && selectionIndex != -1)
+			{
+				textSelection = selectionIndex;
+				richTextBox1.Select(selectionIndex, 0);
+				richTextBox1.ScrollToCaret();
+			}
+
+			isDoubleClick = false;
+		}
+
+		private void treeView1_BeforeCollapse(object sender, TreeViewCancelEventArgs e)
+		{
+			if (isDoubleClick && e.Action == TreeViewAction.Collapse)
+				e.Cancel = true;
+		}
+
+		private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+		{
+			if (isDoubleClick && e.Action == TreeViewAction.Expand)
+				e.Cancel = true;
+		}
+
+		private void treeView1_MouseDown(object sender, MouseEventArgs e)
+		{
+			isDoubleClick = e.Clicks > 1;
+		}
+
+		private void textBox1_KeyDown(object sender, KeyEventArgs e)
+		{
+		}
+
+		private void richTextBox1_VScroll(object sender, EventArgs e)
+		{
+			int firstVisibleChar = richTextBox1.GetCharIndexFromPosition(new Point(0, 0));
+			int lineIndex = richTextBox1.GetLineFromCharIndex(firstVisibleChar);
+
+			if (previousLineIndex != lineIndex)
+			{
+				previousLineIndex = lineIndex;
+				richTextBox2.SelectionStart = richTextBox2.GetFirstCharIndexFromLine(lineIndex);
+				richTextBox2.ScrollToCaret();
+			}
+		}
+
+		private void richTextBox1_ContentsResized(object sender, ContentsResizedEventArgs e)
+		{
+			richTextBox2.ZoomFactor = richTextBox1.ZoomFactor;
+		}
+	}
+
+	public static class Path
+	{
+		public static string GetPath()
+		{
+			var path = AppDomain.CurrentDomain.BaseDirectory.Replace("bin\\Debug\\", "App_Data\\") + "notes.rtf";
+
+			if (File.Exists(path))
+			{
+				return path;
+			}
+			else
+			{
+				File.Create(path);
+			}
+
+			return path;
+		}
+	}
+
+	public class Line
+	{
+		public int Index { get; set; }
+		public string Text { get; set; }
+	}
+}
